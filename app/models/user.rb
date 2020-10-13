6d7bad1a7bd7c5d201a6e440ec9552bc715c8d74
ryo1997 <youarethexbox360@gmail.com>
2020-10-14 01:05:25 +0900

    userモデルの単体テストコード、バリデーション変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,14 @@ class User < ApplicationRecord
 # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 validates :nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday , presence: true
-PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]{8,}+\z/i.freeze
-validates_format_of :password, with: PASSWORD_REGEX
-with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, } do
-  validates :first_name
-  validates :last_name
-  validates :first_name_kana
-  validates :last_name_kana
-  end
+PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
+with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
+       validates :first_name
+       validates :last_name
+     end
+with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: '全角文字を使用してください' } do
+       validates :first_name_kana
+       validates :last_name_kana
+     end
 end
