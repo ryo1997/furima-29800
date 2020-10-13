2aafc40d03e7f836b4eb454bcbc486ab1ebb46ec
ryo1997 <youarethexbox360@gmail.com>
2020-10-13 17:54:55 +0900

    名前に関するバリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nickname, :email, :encrypted_password, :first_name, :last_name, 
-            :first_name_kana, :last_name_kana, :birthday , presence: true
-  
+  validates :nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday , presence: true
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
+       validates :first_name
+       validates :last_name
+       validates :first_name_kana
+       validates :last_name_kana
+     end
 end
