a48f6a1d16336b597850676aa5846bb867e8c59c
ryo1997 <youarethexbox360@gmail.com>
2020-10-14 01:07:36 +0900

    rubocop実行

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              {"aa"}
-    email                 {"aa@aa"}
-    password              {"aa1234"}
-    password_confirmation {"aa1234"}
-    first_name            {"阿部"}
-    last_name             {"太"}
-    first_name_kana       {"アベ"}
-    last_name_kana        {"フトシ"}
-    birthday              {'1930-01-01'}
+    nickname              { 'aa' }
+    email                 { 'aa@aa' }
+    password              { 'aa1234' }
+    password_confirmation { 'aa1234' }
+    first_name            { '阿部' }
+    last_name             { '太' }
+    first_name_kana       { 'アベ' }
+    last_name_kana        { 'フトシ' }
+    birthday              { '1930-01-01' }
   end
 end
