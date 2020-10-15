b28950fb06f100db3576c7717a38ce637570a81e
ryo1997 <youarethexbox360@gmail.com>
2020-10-15 19:28:56 +0900

    Activehashでジャンル作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,17 @@
 class ItemsController < ApplicationController
+  before_action :move_to_root, only: [:new]
 
   def index
   end
-  
+
   def new
+  end
+
+  private
+  def move_to_root
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
   end
 end
 
