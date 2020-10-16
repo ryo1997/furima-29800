8637a0763f0927bcbf3a5d9a18979f5be5848853
ryo1997 <youarethexbox360@gmail.com>
2020-10-16 21:20:25 +0900

    出品した商品の保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,6 +5,16 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
@@ -13,8 +23,8 @@ class ItemsController < ApplicationController
       redirect_to new_user_session_path
     end
   end
-  def items_params
-    params.require(:items).permit(:content, :image).merge(user_id: current_user.id)
+  def item_params
+    params.require(:item).permit( :image, :name, :description, :genre_id, :condition_id, :shipping_charge_id, :prefecture_id, :preparation_day_id, :price ).merge(user_id: current_user.id)
   end
 end
 
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,14 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :genre, :condition, :shipping_charge, :prefecture, :preparation_day
+  belongs_to_active_hash :genre
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :shipping_charge
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :preparation_day
   has_one_attached :image
+  
+  belongs_to :user
+  has_one :purchaser
 
   with_options presence: true do
     validates :name
@@ -15,11 +22,11 @@ class Item < ApplicationRecord
     validates :preparation_day_id
   end
 
-  with_options numericality: { other_than: 1 } true do
-    validates :genre_id
-    validates :condition_id
-    validates :shipping_charge_id
-    validates :prefecture_id
-    validates :preparation_day_id
-  end
+  validates :genre_id, :condition_id, :shipping_charge_id, :prefecture_id, :preparation_day_id, numericality: { other_than: 1 }
+
+  validates :price, format: { with: /\A[0-9]+\z/, message: '半角数字で入力' } 
+
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 end
+
+
