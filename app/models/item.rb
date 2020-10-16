a5bf8f04229a7e7a8f24bf6b4d795b71bb772b06
ryo1997 <youarethexbox360@gmail.com>
2020-10-16 21:33:48 +0900

    エラーメッセージ変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,7 +6,7 @@ class Item < ApplicationRecord
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :preparation_day
   has_one_attached :image
-  
+
   belongs_to :user
   has_one :purchaser
 
@@ -24,7 +24,7 @@ class Item < ApplicationRecord
 
   validates :genre_id, :condition_id, :shipping_charge_id, :prefecture_id, :preparation_day_id, numericality: { other_than: 1 }
 
-  validates :price, format: { with: /\A[0-9]+\z/, message: '半角数字で入力' } 
+  validates :price, format: { with: /\A[0-9]+\z/ } 
 
   validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 end
