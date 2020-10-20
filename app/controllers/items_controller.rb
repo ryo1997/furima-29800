ea25bb5b20d4c4405a779e0b73151540438df868
ryo1997 <youarethexbox360@gmail.com>
2020-10-20 12:38:07 +0900

    アクションの条件分岐追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,8 +20,10 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item.destroy
-    redirect_to root_path
+    if current_user.id == @item.user.id
+      @item.destroy
+    end
+      redirect_to root_path
   end
 
   def show
